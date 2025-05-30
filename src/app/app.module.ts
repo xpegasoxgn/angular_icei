2a778b23c942429f991fe103899fe91c0f6c4697
V1cente12 <jvicentecv@gmail.com>
2025-05-30 19:47:48 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { HomeComponent } from './components/pages/home/home.component';
+import { MainLayoutComponent } from './components/layout/main-layout/main-layout.component';
+import { PerfilComponent } from './components/pages/perfil/perfil.component';
+import { TopBarComponent } from './components/layout/top-bar/top-bar.component';
+import { SidebarComponent } from './components/layout/sidebar/sidebar.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainLayoutComponent,
+    PerfilComponent,
+    TopBarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/pages/perfil/perfil.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil',
+  standalone: false,
+  templateUrl: './perfil.component.html',
+  styleUrl: './perfil.component.css'
+})
+export class PerfilComponent {
+
+}
